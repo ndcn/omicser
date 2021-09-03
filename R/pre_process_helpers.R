4becff6413c7e30d90abedca15ccc8e82a4ca389
Andy Henrie <henrie@datatecnica.com>
2021-09-03 14:00:23 -0700

    add pre-process-helpers to repository

---
+++ R/pre_process_helpers.R
@@ -0,0 +1,6 @@
+# this is a collection of functions to help with pre-processing/curating data
+#
+source("ingest/helper_functions/compute_de_table.R")
+source("ingest/helper_functions/create_config_table.R")
+source("ingest/helper_functions/pack_anndata.R")
+source("ingest/helper_functions/setup_database.R")
