c4c6a28064e66944017aab7de90ce040dec77d07
Kate Hertweck <k8hertweck@gmail.com>
2021-10-13 10:57:19 -0700

    streamlining install

--- examples/install_script.R
+++ examples/install_script.R
@@ -1,18 +1,17 @@
+#### Software installation ####
 
+## These steps follow vignettes 1-Environment Setup and 2-Installation
 
+# install R dependencies
 install.packages("devtools")
 install.packages("reticulate")
 
+# install and configure environment
 reticulate::install_miniconda()
-reticulate::conda_create("omxr",python_version = 3.9)
+reticulate::conda_create("omxr", python_version = 3.9)
 reticulate::conda_install(envname="omxr",
                           channel = "conda-forge",
                           packages = c("scanpy","leidenalg") )
 
-
-# execute THIS:
+# install omicser package
 devtools::install_github("ndcn/omicser")
-
-
-
-
