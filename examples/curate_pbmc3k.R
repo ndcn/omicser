7ac16ca28d242a830d3c20689652b5a8c29090d9
Kate <k8hertweck@gmail.com>
2021-10-14 14:03:42 -0700

    fully executable pbmc3k

--- examples/curate_pbmc3k.R
+++ examples/curate_pbmc3k.R
@@ -1,69 +1,57 @@
-# curate pbmnc3k
-#
-#
-## The data consist of *3k PBMCs from a Healthy Donor* and are freely available from 10x Genomics
-# ([here](http://cf.10xgenomics.com/samples/cell-exp/1.1.0/pbmc3k/pbmc3k_filtered_gene_bc_matrices.tar.gz)
-# from this [webpage](https://support.10xgenomics.com/single-cell-gene-expression/datasets/1.1.0/pbmc3k)).
-#
-#  last line creates a directory for writing processed data.
-#  We will work with the processed PBMC3K dataset. This is a dataset of around
-#3000 peripheral blood mononuclear cells that was produced by 10x Genomics and
-# has been processed as described in the scanpy PBMC3K tutorial.
+#### Create an app to browse PBMC3k data from 10X Genomics ####
 
-#
+#### Environment setup ####
 
+library(devtools) # to install omicser package
+library(reticulate) # runs python when curating data
+library(fs) # file management
 
-DB_NAME <-  list("10x PBMC3k" = "pbmc3k")
+# name of conda environment
+CONDA_ENV <- "omxr"
 
-require(omicser)
-OMICSER_RUN_DIR <- file.path(getwd(),"examples")
-setwd(OMICSER_RUN_DIR) # incase we have relative dir
-# BOOTSTRAP the options we have already set up...
-# NOTE: we are looking in the "quickstart" folder.  the default is to look for the config in with default getwd()
+# install and configure environment
+reticulate::install_miniconda()
+reticulate::conda_create(CONDA_ENV, python_version = 3.9)
+reticulate::conda_install(envname=CONDA_ENV,
+                          channel = "conda-forge",
+                          packages = c("scanpy","leidenalg") )
+# load conda environment
+reticulate::use_condaenv(condaenv = CONDA_ENV, required = TRUE)
 
+# install omicser package
+devtools::install_github("ndcn/omicser")
 
+# location where data, database, and browser files will be created
+dir_create("omicser_test")
+setwd("omicser_test/")
+OMICSER_RUN_DIR <- getwd() # must be absolute path
+DB_ROOT_PATH <- path(OMICSER_RUN_DIR,"test_db")
+DB_NAME <- list("10x PBMC3k" = "pbmc3k")
+RAW_DIR <- path(OMICSER_RUN_DIR,"raw_data", DB_NAME)
 
-# if you already have an omicser_options.yml for other databases run the following lines
-if (FALSE) {
-  omicser_options <- omicser::get_config(in_path = OMICSER_RUN_DIR)
-  CONDA_ENV <- omicser_options$conda_environment
-  DB_ROOT_PATH <- omicser_options$db_root_path
-  if (! (DB_NAME %in% omicser_options$database_names)){
-    omicser_options$database_names <- c(omicser_options$database_names,DB_NAME)
-    omicser::write_config(omicser_options,in_path = OMICSER_RUN_DIR )
-  }
+#### Download and organize data ###
 
+# create directory structure for data and databases
+dir_create(c(OMICSER_RUN_DIR, path(DB_ROOT_PATH, DB_NAME), RAW_DIR))
+# change paths to make data manipulations easier
+setwd(RAW_DIR)
+# download data
+data_file <- "http://cf.10xgenomics.com/samples/cell-exp/1.1.0/pbmc3k/pbmc3k_filtered_gene_bc_matrices.tar.gz"
+download.file(data_file, "pbmc3k_filtered_gene_bc_matrices.tar.gz")
+# extract all downloaded files
+untar("pbmc3k_filtered_gene_bc_matrices.tar.gz")
+# compress individual files (required for scanpy)
+tar("filtered_gene_bc_matrices/hg19/matrix.mtx.gz", files = "filtered_gene_bc_matrices/hg19/matrix.mtx", compression = "gzip")
+tar("filtered_gene_bc_matrices/hg19/barcodes.tsv.gz", files = "filtered_gene_bc_matrices/hg19/barcodes.tsv", compression = "gzip")
+tar("filtered_gene_bc_matrices/hg19/genes.tsv.gz", files = "filtered_gene_bc_matrices/hg19/genes.tsv", compression = "gzip")
+setwd(OMICSER_RUN_DIR)
 
-} else {
-  CONDA_ENV <- "omxr"
-  DB_ROOT_PATH <- file.path(OMICSER_RUN_DIR,"test_db") #or as `relative path` = "test_db"
+#### Data curation: 1. Metadata documentation ####
 
-  omicser_options <- list(
-      conda_environment = CONDA_ENV,
-      db_root_path = DB_ROOT_PATH,
-      database_names = DB_NAME)
+# load browser app package (should also load dependencies)
+library(omicser)
 
-}
-
-
-
-DB_DIR = file.path(DB_ROOT_PATH,DB_NAME)
-if (!dir.exists(DB_DIR)) {
-  dir.create(DB_DIR)
-}
-
-reticulate::use_condaenv(
-  required = TRUE,
-  condaenv =  CONDA_ENV,
-  conda = "auto"
-)
-
-
-#==== 1. documentation / provenance ==============================================================
-# TODO:  markdown file or html with some copy about the database
-#  - lab, paper link/name
-#  summarize results / data origin whatever
-
+# aggregate metadata for dataset
 db_meta <- list(
   organism = 'human',
   lab = "",
@@ -77,158 +65,125 @@ db_meta <- list(
   url = "https://support.10xgenomics.com/single-cell-gene-expression/datasets/1.1.0/pbmc3k"
 )
 
-write_db_meta(db_meta,DB_NAME, db_root = DB_ROOT_PATH)
+# write metadata configuration
+write_db_meta(db_meta, DB_NAME, db_root = DB_ROOT_PATH)
 
-# ALSO create an `additional_info.Rmd` as in `inst/app/www/additional_info.Rmd`
-#==== 2. helper functions =================================================================================
+#### Data curation 2. Raw data ingest ####
 
-
-#==== 3. load data -========================================================================================
-
-
-RAW_DIR <- file.path(OMICSER_RUN_DIR,"raw_data", "pbmc3k")
-if (!dir.exists(RAW_DIR))  dir.create(RAW_DIR)
-
-data_file <- 'http://cf.10xgenomics.com/samples/cell-exp/1.1.0/pbmc3k/pbmc3k_filtered_gene_bc_matrices.tar.gz'
-download.file(data_file, file.path(RAW_DIR,'pbmc3k_filtered_gene_bc_matrices.tar.gz'))
-system( "tar -xzf raw_data/pbmc3k/pbmc3k_filtered_gene_bc_matrices.tar.gz" )
-
-
-#
-# The data consist of *3k PBMCs from a Healthy Donor* and are freely available from 10x Genomics
-# ([here](http://cf.10xgenomics.com/samples/cell-exp/1.1.0/pbmc3k/pbmc3k_filtered_gene_bc_matrices.tar.gz)
-# from this [webpage](https://support.10xgenomics.com/single-cell-gene-expression/datasets/1.1.0/pbmc3k)).
-# On a unix system, you can uncomment and run the following to download and unpack the data. The
-#  last line creates a directory for writing processed data.
-#f we will work with the processed PBMC3K dataset. This is a dataset of around
-#3000 peripheral blood mononuclear cells that was produced by 10x Genomics and
-# has been processed as described in the scanpy PBMC3K tutorial.
-#
-#https://github.com/theislab/scanpy-tutorials/blob/master/pbmc3k.ipynb
-#
-#  # !mkdir data
-# !wget http://cf.10xgenomics.com/samples/cell-exp/1.1.0/pbmc3k/pbmc3k_filtered_gene_bc_matrices.tar.gz -O raw_data/pbmc3k_filtered_gene_bc_matrices.tar.gz
-# !cd data; tar -xzf pbmc3k_filtered_gene_bc_matrices.tar.gz
-# !mkdir write
-#
+# make scanpy functions available
 sc <- reticulate::import("scanpy")
 
-# Load the PBMC dataset
-adata = sc$read_10x_mtx(
-  'raw_data/pbmc3k/filtered_gene_bc_matrices/hg19/',  # the directory with the `.mtx` file
-  var_names='gene_symbols',                # use gene symbols for the variable names (variables-axis index)
-  cache=TRUE)                              # write a cache file for faster subsequent reading
+# load the PBMC dataset
+adata <- sc$read_10x_mtx(
+  # locate directory containing mtx file
+  "raw_data/pbmc3k/filtered_gene_bc_matrices/hg19/",
+  # use gene symbols for the variable names (variables-axis index)
+  var_names='gene_symbols',
+  # write a cache file for faster subsequent reading
+  cache=TRUE)
+# write the file to an object
+adata$write_h5ad(path(DB_ROOT_PATH, DB_NAME,"core_data.h5ad"))
 
-adata$write_h5ad(filename=file.path(DB_ROOT_PATH,DB_NAME,"core_data.h5ad"))
+#### Data curation 3. Format as AnnData ####
 
-########### this is filtered but not many fields to "browse"
-# data_file <- 'https://raw.githubusercontent.com/chanzuckerberg/cellxgene/main/example-dataset/pbmc3k.h5ad'
-# download.file(data_file, file.path(RAW_DIR,'pbmc3k.h5ad'))
+# identify location of raw data
+data_list <- list(object=path(DB_ROOT_PATH,DB_NAME,"core_data.h5ad"))
 
+# create database formatted as AnnData
+adata <- omicser::setup_database(database_name = DB_NAME,
+                                 db_path = DB_ROOT_PATH,
+                                 data_in = data_list,
+                                 db_meta = NULL,
+                                 re_pack = TRUE)
 
-#==== 4. pack into anndata =========================================================================
+#### Data curation 4. Post-processing ####
 
-data_list <- list(object=file.path(DB_ROOT_PATH,DB_NAME,"core_data.h5ad"))
+adata$var_names_make_unique()
+# unnecessary if using `var_names='gene_ids'` in `sc.read_10x_mtx`
 
-adata <- omicser::setup_database(database_name=DB_NAME,
-                              db_path=DB_ROOT_PATH,
-                              data_in=data_list,
-                              db_meta=NULL ,
-                              re_pack=TRUE)
-
-#adata <- anndata::read_h5ad(file.path(RAW_DIR,'pbmc3k.h5ad'))
-
-
-
-# sc <- reticulate::import("scanpy")
-# ad = sc$datasets$pbmc3k_processed()
-
-#==== 5. post processing =========================================================================               --
-
-### WARNING:  but if you re-read the object from the newly written file vs. keep using...
-#
-
-adata$var_names_make_unique()  # this is unnecessary if using `var_names='gene_ids'` in `sc.read_10x_mtx`
-
+# filter data
 sc$pp$filter_cells(adata, min_genes=200)
 sc$pp$filter_genes(adata, min_cells=10)
 
-adata$var['mt'] = startsWith(adata$var_names,'MT-')  # annotate the group of mitochondrial genes as 'mt'
+# annotate the group of mitochondrial genes as 'mt'
+adata$var['mt'] <- startsWith(adata$var_names,'MT-')
 sc$pp$calculate_qc_metrics(adata, qc_vars=list('mt'), percent_top=NULL, log1p=FALSE, inplace=TRUE)
 
-
-adata = adata[adata$obs$n_genes_by_counts < 2500, ]
-adata = adata[adata$obs$pct_counts_mt < 5, ]
+# filter data
+adata <- adata[adata$obs$n_genes_by_counts < 2500, ]
+adata <- adata[adata$obs$pct_counts_mt < 5, ]
 sc$pp$normalize_total(adata, target_sum=1e4)
 sc$pp$log1p(adata)
 sc$pp$highly_variable_genes(adata, min_mean=0.0125, max_mean=3, min_disp=0.5)
 
-adata$raw = adata$copy()
-
-
+# transform data
 #adata = adata[, adata$var$highly_variable]
 sc$pp$regress_out(adata, list('total_counts', 'pct_counts_mt'))
 sc$pp$scale(adata, max_value=10)
 
+# choose top 40 genes by variance across dataset as "targets"
 adata$var$var_rank <- order(adata$var$dispersions_norm)
-# choose top 40 genes by variance across dataset as our "targets"
 target_omics <- adata$var_names[which(adata$var$var_rank <= 40)]
 
-
+# calculate deciles
 adata$var$decile <- dplyr::ntile(adata$var$dispersions_norm, 10)
 #raw <- ad$raw$to_adata()
 
-
-if (FALSE){ # save an intermediate file (incase we want to revert...)
+# save intermediate database file
+if (FALSE){
   adata$write_h5ad(filename=file.path(DB_ROOT_PATH,DB_NAME,"normalized_data.h5ad"))
 }
-#==== 5-a. dimension reduction - PCA / umap  =========================================================================
-# ## Step 2: Normalize with a very vanilla recipe
-# normalized_data = sc$pp$recipe_seurat(raw, copy=TRUE)
 
-## Step 3: Do some basic preprocessing to run PCA and compute the neighbor graph
+## Dimension reduction - PCA / umap
+#run PCA
 sc$pp$pca(adata)
+# compute neighbor graph
 sc$pp$neighbors(adata)
-
-## Step 4: Infer clusters with the Louvain algorithm
-#sc$tl$louvain(ad_tmp)
+## infer clusters
 sc$tl$leiden(adata)
-## Step 5: Compute tsne and umap embeddings
+# compute umap
 sc$tl$umap(adata)
-#ad_tmp$layers <- list(non_regressed=ad$X) #list('count'=layers)
 
-if (FALSE){ # save an intermediate file (incase we want to revert...)
+# save intermediate database file
+if (FALSE){
   adata$write_h5ad(filename=file.path(DB_ROOT_PATH,DB_NAME,"norm_data_plus_dr.h5ad"))
 }
 
-#==== 6. differential expression =========================================================================
+#### Data curation 5. Differential expression ####
 
-
+# identify stats
 test_types <- c('wilcoxon','t-test_overestim_var')
-
-
 comp_types <- c("grpVrest")
 obs_names <- c('leiden')
+# calculate DE
 diff_exp <- omicser::compute_de_table(adata,comp_types, test_types, obs_names,sc)
 
-if (FALSE){ # save an intermediate file (incase we want to revert...)
+# save intermediate database file
+if (FALSE){
   adata$write_h5ad(filename=file.path(DB_ROOT_PATH,DB_NAME,"norm_data_with_de.h5ad"))
 }
 
+# save DE tables
 saveRDS(diff_exp, file = file.path(DB_ROOT_PATH,DB_NAME, "db_de_table.rds"))
 
+#### Data curation 6. Write database ####
 
+# write final database
+adata$write_h5ad(filename=file.path(DB_ROOT_PATH,DB_NAME,"db_data.h5ad"))
 
-#==== 7. create configs =========================================================================
-if (FALSE) { # load intermediate files if available
+#### Configuration 1. Database configuration ####
+
+# load intermediate files if available
+if (FALSE) {
   adata <- anndata::read_h5ad(filename=file.path(DB_ROOT_PATH,DB_NAME,"norm_data_with_de.h5ad"))
   diff_exp <- readRDS( file = file.path(DB_ROOT_PATH,DB_NAME, "db_de_table.rds"))
 }
 
-# what ad$obs do we want to make default values for...
+# set default values for ad$obs
 default_factors <- c("leiden")
 
-# differentials  #if we care we need to explicitly state. defaults will be the order...
+# differentials
+# if we care we need to explicitly state. defaults will be the order...
 config_list <- list(
   x_obs = c("leiden"),
   y_obs =  c('n_genes', 'n_genes_by_counts', 'total_counts', 'total_counts_mt', 'pct_counts_mt'), #MEASURES
@@ -263,31 +218,26 @@ config_list <- list(
                    'mean_counts', 'pct_dropout_by_counts', 'total_counts',
                    'highly_variable', 'means', 'dispersions',
                    'dispersions_norm', 'mean', 'std', 'var_rank', 'decile')
-
 )
 
-
-
+# write db_config.yml
 omicser::write_db_conf(config_list,DB_NAME, db_root = DB_ROOT_PATH)
 
-#==== 8. write data file to load  =========================================================================
-adata$write_h5ad(filename=file.path(DB_ROOT_PATH,DB_NAME,"db_data.h5ad"))
+#### Configuration 2. Browser configuration ####
+
+# Now move to the directory where you want to execute the omicser
+# and make the omicser_options.yml
+
+omicser_options <- list(database_names=DB_NAME,
+                        db_root_path=DB_ROOT_PATH,
+                        conda_environment=CONDA_ENV)
+
+omicser::write_config(omicser_options,in_path = OMICSER_RUN_DIR )
 
 
-
-
-#==== 9. create meta-data narrative for rendering in INGEST  =========================================================================
-
+#==== create meta-data narrative for rendering in INGEST  =========================================================================
 
 # ALSO create an `additional_info.Rmd` as in `inst/app/www/additional_info.Rmd`
-
-
-
-
-
-
-
-
 
 # ALTERNATE SEURAT STYLE =================
 #'
