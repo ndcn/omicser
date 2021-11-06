67d669b4ef365f8e5d881e30e11e8d9319bf5051
Andy Henrie <arrrgo@gmail.com>
2021-11-06 00:33:06 -0700

    finalizing renv

--- dev/config_renv.R
+++ dev/config_renv.R
@@ -37,24 +37,6 @@ renv::init()
 #                           packages = c("scanpy","leidenalg") )
 
 
-CONDA_ENV <- "omxr" #"/Users/ahenrie/Library/r-miniconda/envs/omxr/bin/python"
-CONDA_ENV <-"/Users/ahenrie/Library/r-miniconda/envs/omxr" # avoide sicne we have two omxr
-CONDA_EXE <- "/Users/ahenrie/Library/r-miniconda/bin/conda" #reticulate::conda_binary()
-
-#CONDA_PY_PATH: "/Users/ahenrie/Library/r-miniconda/envs/omxr/bin/python"
-renv::use_python(python = "/Users/ahenrie/Library/r-miniconda/envs/omxr/bin/python", type = "conda")
-# can i encapsulate a miniconda in the environment???
-#reticulate::install_miniconda()
-
-
-
-# conda environment is now encapsulated in teh renv...
-CONDA_ENV <-"renv/python/condaenvs/renv-python" # avoide sicne we have two omxr
-
-reticulate::conda_install(envname=CONDA_ENV,
-                          channel = "conda-forge",
-                          packages = c("scanpy","leidenalg") )
-
 #configure python
 # "/opt/anaconda3/envs/base39/bin/python"
 py <- reticulate::conda_list()$python[7]
@@ -65,7 +47,8 @@ py <- reticulate::conda_list()$python[7]
 cmd <- c("/Users/ahenrie/Library/r-miniconda/bin/conda create -y -p /Users/ahenrie/Projects/NDCN_dev/omicser/.pyenv python=3.9 pip",
           "/Users/ahenrie/Library/r-miniconda/bin/conda install -p /Users/ahenrie/Projects/NDCN_dev/omicser/.pyenv -y seaborn scikit-learn statsmodels numba pytables ",
           "/Users/ahenrie/Library/r-miniconda/bin/conda install  -p /Users/ahenrie/Projects/NDCN_dev/omicser/.pyenv -y -c conda-forge python-igraph leidenalg",
-          "/Users/ahenrie/Projects/NDCN_dev/omicser/.pyenv/bin/python -m pip install scanpy")
+          "/Users/ahenrie/Projects/NDCN_dev/omicser/.pyenv/bin/python -m pip install scanpy",
+         "/Users/ahenrie/Library/r-miniconda/bin/conda env export -p /Users/ahenrie/Projects/NDCN_dev/omicser/.pyenv > environment.yml")
 
 
 # so we'll do everything in conda instead with conda...  possibly doing it in stages makes it faster or less prone to failure... scapyy 1.7.2
@@ -74,10 +57,17 @@ cmd <- c("/Users/ahenrie/Library/r-miniconda/bin/conda create -y -p /Users/ahenr
          "/Users/ahenrie/Library/r-miniconda/bin/conda install  -p /Users/ahenrie/Projects/NDCN_dev/omicser/.pyenv -y -c conda-forge python-igraph leidenalg",
          "/Users/ahenrie/Library/r-miniconda/bin/conda install  -p /Users/ahenrie/Projects/NDCN_dev/omicser/.pyenv -y -c bioconda scanpy")
 
-
 base::system(command = paste(cmd, collapse = " && "))
 
+# that doesn't work either... we need to do it al lat once... in stages makes it faster or less prone to failure... scapyy 1.7.2
+cmd <- c("/Users/ahenrie/Library/r-miniconda/bin/conda create -y -p /Users/ahenrie/Projects/NDCN_dev/omicser/.pyenv python=3.9",
+            "pip seaborn scikit-learn statsmodels numba pytables python-igraph leidenalg scanpy",
+            "-c conda-forge -c bioconda ", "&& /Users/ahenrie/Library/r-miniconda/bin/conda env export -p /Users/ahenrie/Projects/NDCN_dev/omicser/.pyenv > environment.yml")
 
+base::system(command = paste(cmd, collapse = " "))
+
+# python management fails with renv... so probably the first way is the "correct way to install things
+#
 #
 
 CONDA_ENV <- "" #"/Users/ahenrie/Library/r-miniconda/envs/omxr/bin/python"
@@ -87,25 +77,18 @@ reticulate::use_condaenv( required = TRUE,
                           condaenv = CONDA_ENV,
                           conda = CONDA_EXE)
 #conirm its active.
-#
 
-# virtualevn is NOT working because the pip installations on Mac need special compiler flags for scanpy...
-reticulate::conda_create(envname = "./.pyenv",python_version = 3.9,
-                              packages = c("pip","wheel","setuptools") )
-
-reticulate::use_condaenv("./.pyenv")
-
-reticulate::virtualenv_install(envname = "./.pyenv", packages = c("'scanpy[leiden]'") )
+reticulate::py_config()
 
 
-# virtualevn is NOT working because the pip installations on Mac need special compiler flags for scanpy...
-reticulate::virtualenv_create(envname = "./.pyenv",
-                              python = "python3.9",
-                              packages = c("pip","wheel","setuptools"))
+CONDA_ENV <- "omxr" #"/Users/ahenrie/Library/r-miniconda/envs/omxr/bin/python"
+CONDA_ENV <-"/Users/ahenrie/Library/r-miniconda/envs/omxr" # avoide sicne we have two omxr
+CONDA_EXE <- "/Users/ahenrie/Library/r-miniconda/bin/conda" #reticulate::conda_binary()
 
-reticulate::use_virtualenv("./.pyenv")
-
-reticulate::virtualenv_install(envname = "./.pyenv", packages = c("'scanpy[leiden]'") )
+#CONDA_PY_PATH: "/Users/ahenrie/Library/r-miniconda/envs/omxr/bin/python"
+renv::use_python(python = "/Users/ahenrie/Library/r-miniconda/envs/omxr/bin/python", type = "conda")
+# can i encapsulate a miniconda in the environment???
+#reticulate::install_miniconda()
 
 
 reticulate::py_config()
