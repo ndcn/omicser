5efeeba9d5e1e398049ff8f56ff372712a11ebf5
Andy Henrie <arrrgo@gmail.com>
2021-11-23 10:42:08 -1000

    finished cleanup... v0.1 ready

---
+++ examples/run_browser_script.R
@@ -0,0 +1,29 @@
+#Run browser script
+DB_ROOT_PATH <- "/Users/ahenrie/Projects/NDCN_dev/omicser/quickstart/test_db"
+
+# read from the app_config.yml
+omicser::run_defaults()
+
+
+
+# run in system default browser (chrome recommended) with databases found in DB_ROOT_PATH
+omicser::run_in_browser(
+           db_root = DB_ROOT_PATH,
+           database_names = list(UNDEFINED="UNDEFINED"),
+           install_type = "arguments"
+   )
+
+
+# run in system default browser with databases found in DB_ROOT_PATH
+omicser::run_in_browser(
+  db_root = DB_ROOT_PATH,
+  database_names = list(`Domenico DIA`="domenico_stem_cell"),
+  install_type = "arguments"
+)
+
+
+# run in the rstudio browser with databases found in DB_ROOT_PATH, and modal database choice
+omicser::run_app(db_root = DB_ROOT_PATH,
+                   database_names = list(UNDEFINED="UNDEFINED"),
+                   install_type = "INSTALL_TYPE"
+                   )
