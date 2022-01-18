361cfe4e42bea46cf0d17862e553a92c8d425817
Rico Derks <r.j.e.derks@lumc.nl>
2022-01-18 13:09:59 +0100

    Changed to curved upwards pointing arrow to its respective unicode value.

--- R/mod_omic_selector.R
+++ R/mod_omic_selector.R
@@ -44,7 +44,7 @@ mod_omic_selector_ui <- function(id){
       column(
         2,
         style='padding-left:0px; padding-right:1px',
-        actionButton(ns("AB_text_omic_add"),"add ⤴",class = "btn btn-primary" )
+        actionButton(ns("AB_text_omic_add"),"add \u2934",class = "btn btn-primary" )
       )
     ),
 
