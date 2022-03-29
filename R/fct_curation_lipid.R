97a8053b08aa848e9a4d9f65349180a6d9665830
Rico Derks <r.j.e.derks@lumc.nl>
2022-03-29 15:36:15 +0200

    Added check_database_name function.

--- R/fct_curation_lipid.R
+++ R/fct_curation_lipid.R
@@ -68,3 +68,50 @@ read_lipid_data <- function(filename = NULL,
   # return the result
   return(res)
 }
+
+
+#' @title Check the database name
+#'
+#' @description Check if the database name doesn't contain any spaces or special
+#'     characters.
+#'
+#' @param db_name character vector (length = 1) with the database name.
+#' @param current_db_names named list with all the current database names.
+#'
+#' @return TRUE if database name is ok, FALSE if not
+#'
+#' @author Rico Derks
+#'
+#' @noRd
+#'
+check_database_name <- function(db_name = NULL,
+                                current_db_names = NULL) {
+
+  # initialize the list to return
+  res <- list(status = NULL,
+              message = NULL)
+
+  # check if the database doesn't contain any special characters
+  db_name_matches <- regexpr(text = db_name,
+                             pattern = "[a-zA-Z//-_]+")
+
+  if(nchar(db_name) == attributes(db_name_matches)$match.length) {
+    # all ok
+    res$status <- "ok"
+  } else {
+    # special character used
+    res$status <- "error"
+    res$message <- "Error in database name. Please only use 'a-z', 'A-Z' or '-_'!"
+  }
+
+  # check if database name exists
+  # this is only done for database known to omicser
+  # other folders are not detected
+  if(db_name %in% unlist(current_db_names)) {
+    res$status <- "error"
+    res$message <- "Database name already exists!"
+  }
+
+  # return the result
+  return(res)
+}
